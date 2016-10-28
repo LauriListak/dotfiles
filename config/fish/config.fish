59235d631f9e60318f67ce0c7bb65616b745529b
Lauri Listak <lauri@listak.net>
2016-10-28 22:55:11 +0300

    Try fix for UTF-8 powerline

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,7 +8,7 @@ set __fish_git_prompt_color_branch yellow
 set __fish_git_prompt_char_dirtystate '⚡'
 set __fish_git_prompt_char_stagedstate '→'
 set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '↑'
+set __fish_git_prompt_char_upstream_ahead '>' '↑'
 set __fish_git_prompt_char_upstream_behind '↓'
  
 function fish_prompt
