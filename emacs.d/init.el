c6eb8d0bfa974b517733e23c7fcb6e4a49041134
Lauri Listak <lauri@listak.net>
2016-12-04 22:44:46 +0200

    Emacs custom variables

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -233,3 +233,17 @@
 
 ;; Do not disable upcase-region command
 (put 'upcase-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (haskell-mode ox-reveal latex-preview-pane auctex monokai-theme magit tagedit rainbow-delimiters projectile smex ido-ubiquitous ess ac-cider cider clojure-mode-extra-font-locking clojure-mode paredit))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
